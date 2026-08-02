241df54d534e931f4521a73d0cdf5054828d9ed9
Cameron Hatch <cameronhatch@Camerons-MacBook-Air-2.local>
2026-08-01 20:27:24 -0600

    connect card

--- src/Connect/index.tsx
+++ src/Connect/index.tsx
@@ -1,5 +1,7 @@
 import { Box, Button, Grid, Toolbar, Typography } from '@mui/material'
 
+import ConnectCard from './ConnectCard'
+
 const Connect = () => {
   return (
     <Box>
@@ -10,42 +12,24 @@ const Connect = () => {
         We want to hear from you!
       </Typography>
       <Grid container>
-        <Grid size={{ xs: 12, sm: 6 }}>
-          <Typography
-            sx={{
-              fontSize: '1.25rem',
-              fontWeight: 600,
-              marginBottom: '0.75rem',
-            }}
-          >
-            Facebook
-          </Typography>
-          <Typography sx={{ fontSize: '0.875', fontWeight: 400 }}>
-            Join our community of New Mexico puzzlers!
-          </Typography>
-          <Button
-            href="https://www.facebook.com/groups/1730853644927452"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Let's Connect
-          </Button>
-        </Grid>
-        <Grid size={{ xs: 12, sm: 6 }}>
-          <Typography
-            sx={{
-              fontSize: '1.25rem',
-              fontWeight: 600,
-              marginBottom: '0.75rem',
-            }}
-          >
-            Email
-          </Typography>
-          <Typography sx={{ fontSize: '0.875', fontWeight: 400 }}>
-            Have questions or feedback? Send us an email!
-          </Typography>
-          <Button href="mailto:nmjigsaw.org">Email us!</Button>
-        </Grid>
+        <ConnectCard
+          title="Facebook"
+          content="Join our community of New Mexico puzzlers!"
+          actionItem={
+            <Button
+              href="https://www.facebook.com/groups/1730853644927452"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Let's Connect
+            </Button>
+          }
+        />
+        <ConnectCard
+          title="Email"
+          content="Have questions or feedback? Send us an email!"
+          actionItem={<Button href="mailto:nmjigsaw.org">Email us!</Button>}
+        />
       </Grid>
     </Box>
   )
