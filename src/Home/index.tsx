051739de347df9a485c5a39c4286de60e6a3128e
Cameron Hatch <cameronhatch@Camerons-MacBook-Air-2.local>
2026-08-07 16:43:11 -0600

    text updates

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -10,10 +10,10 @@ const Home = () => {
       </Typography>
       <Typography sx={{ textAlign: 'left' }}>
         We're a group of New Mexico puzzlers who are passionate about growing
-        the puzzle community in the 505. We want to connect fellow disectologies
-        throughout the state by hosting puzzling opportunities, building an
-        online community, and curating a list of puzzle events throughout the
-        state.
+        the puzzle community in the 505. We want to connect fellow New Mexican
+        puzzle enthusiasts the state by hosting puzzling events, building an
+        online community, and curating a list of puzzle events taking place
+        around the state.
       </Typography>
     </Page>
   )
