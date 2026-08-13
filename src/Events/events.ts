d352c405269d692245f67c83e5a167917c3b4d2a
Cameron Hatch <cameronhatch@Camerons-MacBook-Air-2.local>
2026-08-13 17:12:38 -0600

    add october slice events

--- src/Events/events.ts
+++ src/Events/events.ts
@@ -3,7 +3,7 @@ import { PuzzleEvent } from './structs'
 const events: PuzzleEvent[] = [
   {
     title: 'Super Saturday Mini Tourney #6',
-    startsAt: new Date('2026-08-29 15:00:00'),
+    startsAt: new Date('2026-08-29T15:00:00.000Z'),
     city: 'Albuquerque',
     type: 'speed',
     logoName: 'speedPuzzling',
@@ -11,7 +11,7 @@ const events: PuzzleEvent[] = [
   },
   {
     title: 'Puzzle Challenge - WEST',
-    startsAt: new Date('2026-08-06 01:00:00'),
+    startsAt: new Date('2026-08-06T01:00:00.000Z'),
     city: 'Albuquerque',
     type: 'speed',
     logoName: 'sliceAndDice',
@@ -20,7 +20,7 @@ const events: PuzzleEvent[] = [
   },
   {
     title: 'Puzzle Challenge - EAST',
-    startsAt: new Date('2026-08-13 01:00:00'),
+    startsAt: new Date('2026-08-13T01:00:00.000Z'),
     city: 'Albuquerque',
     type: 'speed',
     logoName: 'sliceAndDice',
@@ -29,7 +29,7 @@ const events: PuzzleEvent[] = [
   },
   {
     title: 'Puzzle Challenge - WEST',
-    startsAt: new Date('2026-09-02 01:00:00'),
+    startsAt: new Date('2026-09-03T01:00:00.000Z'),
     city: 'Albuquerque',
     type: 'speed',
     logoName: 'sliceAndDice',
@@ -38,7 +38,7 @@ const events: PuzzleEvent[] = [
   },
   {
     title: 'Puzzle Challenge - EAST',
-    startsAt: new Date('2026-09-10 01:00:00'),
+    startsAt: new Date('2026-09-10T01:00:00.000Z'),
     city: 'Albuquerque',
     type: 'speed',
     logoName: 'sliceAndDice',
@@ -47,19 +47,37 @@ const events: PuzzleEvent[] = [
   },
   {
     title: 'Super Saturday Warm-up',
-    startsAt: new Date('2026-08-22 18:30:00'),
+    startsAt: new Date('2026-08-22T18:30:00.000Z'),
     city: 'Albuquerque',
     type: 'speed',
     infoLink: 'https://www.facebook.com/events/2251442822077446',
   },
   {
     title: 'Puzzle Competition (Rio Rancho Public Libraries)',
-    startsAt: new Date('2026-08-29 19:00:00'),
+    startsAt: new Date('2026-08-29T19:00:00.000Z'),
     city: 'Rio Rancho',
     type: 'speed',
     logoName: 'rioRanchoLibrary',
     infoLink:
       'https://riorancholibraries.events.mylibrary.digital/event?id=261350',
+  },
+  {
+    title: 'Puzzle Challenge - WEST',
+    startsAt: new Date('2026-10-08T01:00:00.000Z'),
+    city: 'Albuquerque',
+    type: 'speed',
+    logoName: 'sliceAndDice',
+    infoLink:
+      'https://sliceanddicepizzeria.com/event/puzzle-challenge-west/2026-10-07/',
+  },
+  {
+    title: 'Puzzle Challenge - EAST',
+    startsAt: new Date('2026-10-15T01:00:00.000Z'),
+    city: 'Albuquerque',
+    type: 'speed',
+    logoName: 'sliceAndDice',
+    infoLink:
+      'https://sliceanddicepizzeria.com/event/puzzle-challenge-east/2026-10-14/',
   },
 ]
 
